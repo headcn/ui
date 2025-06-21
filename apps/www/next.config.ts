f8cac16c7c42d0c1c38c8a40241ba297410e99c0
stabldev <114811070+stabldev@users.noreply.github.com>
2025-06-21 19:48:22 +0530

    chore: update next config and deploy ci/cd

--- apps/www/next.config.ts
+++ apps/www/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  // enable static export
+  output: "export",
+  distDir: "../../dist/apps/www",
+  basePath: "/ui",
 };
 
 export default nextConfig;
