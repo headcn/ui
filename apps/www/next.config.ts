22d052f6cf4d1c736bed0981ec91b1aed6f729e6
stabldev <114811070+stabldev@users.noreply.github.com>
2025-06-21 21:58:18 +0530

    fix: add production-only next app config

--- apps/www/next.config.ts
+++ apps/www/next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // production-only settings
+  ...(process.env.NODE_ENV === "production" && {
+    output: "export",
+    distDir: "../../dist/apps/www",
+  }),
+  // shared
   devIndicators: false,
-  // enable static export
-  output: "export",
-  distDir: "../../dist/apps/www",
   basePath: "/ui",
 };
 
