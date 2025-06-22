af57430bf83d34024db01ea82cd0fb68911014c4
Stabldev <114811070+stabldev@users.noreply.github.com>
2025-06-22 17:58:37 +0530

    feat: `Button` component (#22)
    * chore: update favicon and metadata
    
    * feat: add theme, cva and basic button
    
    * feat: test button of diff size
    
    * feat: add heroicons and add icon case
    
    * feat: button icon variant
    
    * feat: add more button variants
    
    * chore: remove input color

--- apps/www/src/app/page.tsx
+++ apps/www/src/app/page.tsx
@@ -1,3 +1,19 @@
+import { Button } from "@/registry/ui/button";
+import { CheckCircleIcon } from "@heroicons/react/16/solid";
+
 export default function Home() {
-  return <h1>Headcn UI</h1>;
+  return (
+    <div className="flex items-center justify-center gap-5 h-dvh w-dvw">
+      <Button size={"xs"}>Button</Button>
+      <Button size={"sm"}>Button</Button>
+      <Button size={"icon-md"}>
+        <CheckCircleIcon className="size-4" />
+      </Button>
+      <Button size={"md"} variant={"link"}>
+        <CheckCircleIcon className="size-4" />
+        Button
+      </Button>
+      <Button size={"lg"}>Button</Button>
+    </div>
+  );
 }
---
+++ apps/www/src/registry/ui/button.tsx
@@ -0,0 +1,56 @@
+import { cn } from "@/lib/utils";
+import { Button as HeadlessButton } from "@headlessui/react";
+import type { ComponentProps } from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center whitespace-nowrap gap-1.5 rounded-md text-sm font-semibold shadow-xs data-active:translate-y-0.5 transform transition-all",
+  {
+    variants: {
+      variant: {
+        primary: "bg-primary text-primary-foreground data-hover:bg-primary/90",
+        secondary:
+          "bg-secondary text-secondary-foreground data-hover:bg-secondary/90",
+        destructive:
+          "bg-destructive text-foreground data-hover:bg-destructive/90",
+        outline:
+          "bg-accent/50 text-accent-foreground border data-hover:bg-accent",
+        ghost: "data-hover:bg-accent text-accent-foreground",
+        link: "text-primary data-hover:underline underline-offset-4",
+      },
+      size: {
+        xs: "h-7 px-2.5 text-xs has-[>svg]:px-2",
+        sm: "h-8 px-3 has-[>svg]:px-2.5",
+        md: "h-9 px-4 has-[>svg]:px-3",
+        lg: "h-10 px-5 has-[>svg]:px-4",
+        "icon-xs": "size-7",
+        "icon-sm": "size-8",
+        "icon-md": "size-9",
+        "icon-lg": "size-10",
+      },
+    },
+    defaultVariants: {
+      variant: "primary",
+      size: "md",
+    },
+  },
+);
+
+function Button({
+  children,
+  className,
+  variant,
+  size,
+  ...props
+}: ComponentProps<"button"> & VariantProps<typeof buttonVariants>) {
+  return (
+    <HeadlessButton
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    >
+      {children}
+    </HeadlessButton>
+  );
+}
+
+export { Button, buttonVariants };
