4fdefaf1fd4e145cb967a6e1af48f1d2245e4d3a
stabldev <114811070+stabldev@users.noreply.github.com>
2025-06-22 14:57:40 +0530

    feat: add heroicons and add icon case

--- apps/www/src/app/page.tsx
+++ apps/www/src/app/page.tsx
@@ -1,11 +1,26 @@
 import { Button } from "@/registry/ui/button";
+import { CheckCircleIcon } from "@heroicons/react/16/solid";
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center gap-5 h-dvh w-dvw">
       <Button size={"xs"}>Button</Button>
-      <Button size={"sm"}>Button</Button>
-      <Button size={"md"}>Button</Button>
+      <Button size={"xs"}>
+        <CheckCircleIcon className="size-4" />
+        Button
+      </Button>
+      <Button size={"sm"}>
+        <CheckCircleIcon className="size-4" />
+        Button
+      </Button>
+      <Button size={"md"}>
+        <CheckCircleIcon className="size-4" />
+        Button
+      </Button>
+      <Button size={"lg"}>
+        <CheckCircleIcon className="size-4" />
+        Button
+      </Button>
       <Button size={"lg"}>Button</Button>
     </div>
   );
--- apps/www/src/registry/ui/button.tsx
+++ apps/www/src/registry/ui/button.tsx
@@ -4,17 +4,17 @@ import type { ComponentProps } from "react";
 import { cva, type VariantProps } from "class-variance-authority";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap gap-2 rounded-md text-sm font-semibold shadow-xs data-active:translate-y-0.5 transform transition-all",
+  "inline-flex items-center justify-center whitespace-nowrap gap-1.5 rounded-md text-sm font-semibold shadow-xs data-active:translate-y-0.5 transform transition-all",
   {
     variants: {
       variant: {
         primary: "bg-primary text-primary-foreground data-hover:bg-primary/90",
       },
       size: {
-        xs: "h-7 px-2.5 text-xs",
-        sm: "h-8 px-3",
-        md: "h-9 px-4",
-        lg: "h-10 px-5 text-base",
+        xs: "h-7 px-2.5 text-xs has-[>svg]:px-2",
+        sm: "h-8 px-3 has-[>svg]:px-2.5",
+        md: "h-9 px-4 has-[>svg]:px-3",
+        lg: "h-10 px-5 has-[>svg]:px-4",
       },
     },
     defaultVariants: {
