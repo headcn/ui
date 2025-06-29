146ab28cda969e20dc5fedcf1b1f95416186a003
Stabldev <114811070+stabldev@users.noreply.github.com>
2025-06-29 09:30:21 +0530

    feat(www): update brand logo font-fam (#42)
    * feat(www): update brand logo font-fam
    
    * fix: metadata title default

--- apps/www/app/layout.tsx
+++ apps/www/app/layout.tsx
@@ -2,14 +2,14 @@ import type { Metadata } from "next"
 
 import SiteFooter from "@/components/site-footer"
 import SiteHeader from "@/components/site-header"
-import { inter } from "@/lib/fonts"
+import { boldonse, inter } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
 
 import "@/styles/globals.css"
 
 export const metadata: Metadata = {
   title: {
-    default: "Build beautiful, accessible UI components",
+    default: "Build beautiful, accessible UI components - headcn/ui",
     template: "%s - headcn/ui",
   },
   description: "Headless UI components, beautifully designed and ready to use.",
@@ -39,7 +39,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className={cn(inter.className, "relative bg-black antialiased")}>
+      <body
+        className={cn(
+          inter.className,
+          boldonse.variable,
+          "relative bg-black antialiased"
+        )}
+      >
         <div className="absolute inset-0 top-0 -z-20 bg-[url(/media/bg-top.jpg)] bg-position-[35%_top] bg-no-repeat sm:bg-position-[38%_top] md:bg-position-[40%_top] lg:bg-position-[44%_top] xl:bg-top forced-colors:hidden"></div>
         <div className="absolute inset-0 -z-10 bg-[url(/media/bg-noise.png)] opacity-10 forced-colors:hidden"></div>
         <div className="absolute inset-0 bottom-0 -z-30 bg-[url(/media/bg-bottom.jpg)] bg-position-[35%_bottom] bg-no-repeat mix-blend-screen sm:bg-position-[38%_bottom] md:bg-position-[40%_bottom] lg:bg-position-[44%_bottom] xl:bg-bottom forced-colors:hidden"></div>
