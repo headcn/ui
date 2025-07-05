763241c53e062e7acb080ae8cda8843976015fc4
stabldev <114811070+stabldev@users.noreply.github.com>
2025-07-05 09:57:13 +0530

    feat(www): combobox docs and demo

---
+++ apps/www/registry/examples/combobox-demo.tsx
@@ -0,0 +1,58 @@
+"use client"
+
+import {
+  Combobox,
+  ComboboxButton,
+  ComboboxInput,
+  ComboboxOption,
+  ComboboxOptions,
+} from "@/registry/ui/combobox"
+import { CheckIcon, ChevronDownIcon } from "@heroicons/react/20/solid"
+import { useState } from "react"
+
+const people = [
+  { id: 1, name: "Tom Cook" },
+  { id: 2, name: "Wade Cooper" },
+  { id: 3, name: "Tanya Fox" },
+  { id: 4, name: "Arlene Mccoy" },
+  { id: 5, name: "Devon Webb" },
+]
+
+export default function ComboboxDemo() {
+  const [query, setQuery] = useState("")
+  const [selected, setSelected] = useState(people[1])
+
+  const filteredPeople =
+    query === ""
+      ? people
+      : people.filter((person) => {
+          return person.name.toLowerCase().includes(query.toLowerCase())
+        })
+
+  return (
+    <Combobox
+      value={selected}
+      onChange={(value) => setSelected(value)}
+      onClose={() => setQuery("")}
+    >
+      <div className="relative">
+        <ComboboxInput
+          displayValue={(person) => person?.name}
+          onChange={(event) => setQuery(event.target.value)}
+        />
+        <ComboboxButton>
+          <ChevronDownIcon className="fill-muted-foreground group-data-hover:fill-foreground size-4" />
+        </ComboboxButton>
+      </div>
+
+      <ComboboxOptions anchor="bottom" transition>
+        {filteredPeople.map((person) => (
+          <ComboboxOption key={person.id} value={person}>
+            <div className="text-foreground text-sm/6">{person.name}</div>
+            <CheckIcon className="fill-foreground invisible size-4 group-data-selected:visible" />
+          </ComboboxOption>
+        ))}
+      </ComboboxOptions>
+    </Combobox>
+  )
+}
