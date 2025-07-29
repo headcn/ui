2e210b3077a911e95f5511dadaa069dc8c648b38
stabldev <114811070+stabldev@users.noreply.github.com>
2025-07-29 21:37:53 +0530

    chore: add prettier

--- packages/headcn/src/index.ts
+++ packages/headcn/src/index.ts
@@ -1,18 +1,18 @@
-import { Command } from "commander";
-import { build } from "@/src/commands/build";
-import packageJson from "@/package.json";
+import packageJson from "@/package.json"
+import { build } from "@/src/commands/build"
+import { Command } from "commander"
 
-process.on("SIGINT", () => process.exit(0));
-process.on("SIGTERM", () => process.exit(0));
+process.on("SIGINT", () => process.exit(0))
+process.on("SIGTERM", () => process.exit(0))
 
 async function main() {
   const program = new Command()
     .name("headcn")
     .description("add components and dependencies to your project")
-    .version(packageJson.version || "dev", "-v", "--version");
+    .version(packageJson.version || "dev", "-v", "--version")
 
-  program.addCommand(build);
-  program.parse();
+  program.addCommand(build)
+  program.parse()
 }
 
-main();
+main()
--- packages/headcn/tsup.config.ts
+++ packages/headcn/tsup.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from "tsup"
 
 export default defineConfig({
   entry: ["./src/index.ts", "./src/registry/index.ts"],
@@ -10,4 +10,4 @@ export default defineConfig({
   clean: true,
   outDir: "dist",
   treeshake: true,
-});
+})
