e5daa4d5ade2fd56e1fb297f2048cdef0d5b9a6c
Stabldev <114811070+stabldev@users.noreply.github.com>
2025-07-29 09:45:33 +0530

    chore: upgrade version and fix overflow (#80)

--- apps/www/registry/examples/button-icon.tsx
+++ apps/www/registry/examples/button-icon.tsx
@@ -3,7 +3,7 @@ import { StarIcon } from "@heroicons/react/16/solid"
 
 export default function ButtonIcon() {
   return (
-    <Button size={"icon-md"}>
+    <Button variant={"outline"} size={"icon-md"}>
       <StarIcon className="size-5" />
     </Button>
   )
