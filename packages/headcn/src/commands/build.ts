9bbfd9f78197ef5655c2581b6119080cd829af55
Stabldev <114811070+stabldev@users.noreply.github.com>
2025-08-08 09:02:23 +0530

    refactor(headcn): handle command errors (#90)

--- packages/headcn/src/commands/build.ts
+++ packages/headcn/src/commands/build.ts
@@ -1,5 +1,8 @@
 import { prepareBuild } from "@/src/prepare/prepare-build"
 import { registryItemSchema, registrySchema } from "@/src/registry/schema"
+import { handleError } from "@/src/utils/handle-error"
+import { logger } from "@/src/utils/logger"
+import { spinner } from "@/src/utils/spinner"
 import { Command } from "commander"
 import fs from "fs/promises"
 import path from "path"
@@ -20,48 +23,60 @@ export const build = new Command()
     "./public/r/components"
   )
   .action(async (registry: string, opts) => {
+    const options = buildOptionsSchema.parse({
+      registryFile: registry,
+      outputDir: opts.output,
+    })
+
     try {
-      const options = buildOptionsSchema.parse({
-        registryFile: registry,
-        outputDir: opts.output,
-      })
-
-      const { resolvedPaths } = await prepareBuild(options)
-      const registryContent = await fs.readFile(
-        resolvedPaths.registryFile,
-        "utf-8"
-      )
-
-      const result = registrySchema.safeParse(JSON.parse(registryContent))
-
-      if (!result.success) {
-        console.log(`Invalid registry file at: ${resolvedPaths.registryFile}`)
-        process.exit(1)
-      }
-
-      for (const registryItem of result.data.items) {
-        if (!registryItem.files) continue
-
-        for (const file of registryItem.files) {
-          file["content"] = await fs.readFile(
-            path.join(process.cwd(), file.path),
-            "utf-8"
-          )
-        }
-
-        const result = registryItemSchema.safeParse(registryItem)
-        if (!result.success) {
-          console.log(`Invalid registry item: ${registryItem.name}`)
-          continue
-        }
-
-        await fs.writeFile(
-          path.join(resolvedPaths.outputDir, `${result.data.name}.json`),
-          JSON.stringify(result.data, null, 2)
-        )
-      }
+      await runBuild(options)
     } catch (err) {
-      console.error(err)
-      process.exit(1)
+      handleError(err)
     }
   })
+
+async function runBuild(options: z.infer<typeof buildOptionsSchema>) {
+  const { resolvedPaths } = await prepareBuild(options)
+  const registryContent = await fs.readFile(resolvedPaths.registryFile, "utf-8")
+
+  const result = registrySchema.safeParse(JSON.parse(registryContent))
+
+  if (!result.success) {
+    logger.error(`Invalid registry file at: ${resolvedPaths.registryFile}`)
+    process.exit(1)
+  }
+
+  const buildSpinner = spinner("Building registry.")
+  for (const registryItem of result.data.items) {
+    if (!registryItem.files) continue
+
+    buildSpinner.start(`Building ${registryItem.name}.`)
+
+    for (const file of registryItem.files) {
+      file["content"] = await fs.readFile(
+        path.join(process.cwd(), file.path),
+        "utf-8"
+      )
+    }
+
+    const result = registryItemSchema.safeParse(registryItem)
+    if (!result.success) {
+      logger.error(`Invalid registry item: ${registryItem.name}`)
+      continue
+    }
+
+    await fs.writeFile(
+      path.join(resolvedPaths.outputDir, `${result.data.name}.json`),
+      JSON.stringify(result.data, null, 2)
+    )
+  }
+
+  // copy registry file to public (output dir)
+  await fs.copyFile(
+    resolvedPaths.registryFile,
+    path.resolve(resolvedPaths.outputDir, "registry.json")
+  )
+
+  // bring back spinerrr
+  buildSpinner.succeed("Building registry.")
+}
---
+++ packages/headcn/src/utils/handle-error.ts
@@ -0,0 +1,22 @@
+import { logger } from "@/src/utils/logger"
+import { z } from "zod"
+
+export function handleError(error: unknown) {
+  logger.break()
+  logger.error(
+    "Something went wrong. Please check the error below for more details."
+  )
+  logger.error("If the problem persists, please open an issue on GitHub.")
+  logger.break()
+  if (typeof error === "string") {
+    logger.error(error)
+  } else if (error instanceof z.ZodError) {
+    logger.error("Validation failed:")
+    logger.error(z.treeifyError(error).errors.join("\n"))
+  } else if (error instanceof Error) {
+    logger.error(error.message)
+  }
+
+  logger.break()
+  process.exit(1)
+}
