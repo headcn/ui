94f9bfb52d8793f8452917f9e8c2fe227372ee47
Stabldev <114811070+stabldev@users.noreply.github.com>
2025-08-09 08:00:56 +0530

    chore(www): build registry components (#94)
    Build `registry:ui` and `registry:examples` items for public use.

--- apps/www/registry/registry-examples.ts
+++ apps/www/registry/registry-examples.ts
@@ -12,4 +12,213 @@ export const examples: RegistryItem[] = [
       },
     ],
   },
+  {
+    name: "button-destructive",
+    type: "registry:example",
+    registryDepends: ["button"],
+    files: [
+      {
+        path: "examples/button-destructive.tsx",
+        type: "registry:example",
+      },
+    ],
+  },
+  {
+    name: "button-ghost",
+    type: "registry:example",
+    registryDepends: ["button"],
+    files: [
+      {
+        path: "examples/button-ghost.tsx",
+        type: "registry:example",
+      },
+    ],
+  },
+  {
+    name: "button-icon",
+    type: "registry:example",
+    registryDepends: ["button"],
+    files: [
+      {
+        path: "examples/button-icon.tsx",
+        type: "registry:example",
+      },
+    ],
+  },
+  {
+    name: "button-link",
+    type: "registry:example",
+    registryDepends: ["button"],
+    files: [
+      {
+        path: "examples/button-link.tsx",
+        type: "registry:example",
+      },
+    ],
+  },
+  {
+    name: "button-secondary",
+    type: "registry:example",
+    registryDepends: ["button"],
+    files: [
+      {
+        path: "examples/button-secondary.tsx",
+        type: "registry:example",
+      },
+    ],
+  },
+  {
+    name: "button-with-icon",
+    type: "registry:example",
+    registryDepends: ["button"],
+    files: [
+      {
+        path: "examples/button-with-icon.tsx",
+        type: "registry:example",
+      },
+    ],
+  },
+  {
+    name: "checkbox-demo",
+    type: "registry:example",
+    registryDepends: ["checkbox"],
+    files: [
+      {
+        path: "examples/checkbox-demo.tsx",
+        type: "registry:example",
+      },
+    ],
+  },
+  {
+    name: "combobox-demo",
+    type: "registry:example",
+    registryDepends: ["combobox"],
+    files: [
+      {
+        path: "examples/combobox-demo.tsx",
+        type: "registry:example",
+      },
+    ],
+  },
+  {
+    name: "fieldset-demo",
+    type: "registry:example",
+    registryDepends: ["fieldset"],
+    files: [
+      {
+        path: "examples/fieldset-demo.tsx",
+        type: "registry:example",
+      },
+    ],
+  },
+  {
+    name: "input-demo",
+    type: "registry:example",
+    registryDepends: ["input"],
+    files: [
+      {
+        path: "examples/input-demo.tsx",
+        type: "registry:example",
+      },
+    ],
+  },
+  {
+    name: "input-disabled",
+    type: "registry:example",
+    registryDepends: ["input"],
+    files: [
+      {
+        path: "examples/input-disabled.tsx",
+        type: "registry:example",
+      },
+    ],
+  },
+  {
+    name: "input-with-button",
+    type: "registry:example",
+    registryDepends: ["input"],
+    files: [
+      {
+        path: "examples/input-with-button.tsx",
+        type: "registry:example",
+      },
+    ],
+  },
+  {
+    name: "input-with-label",
+    type: "registry:example",
+    registryDepends: ["input"],
+    files: [
+      {
+        path: "examples/input-with-label.tsx",
+        type: "registry:example",
+      },
+    ],
+  },
+  {
+    name: "listbox-demo",
+    type: "registry:example",
+    registryDepends: ["listbox"],
+    files: [
+      {
+        path: "examples/listbox-demo.tsx",
+        type: "registry:example",
+      },
+    ],
+  },
+  {
+    name: "radio-group-demo",
+    type: "registry:example",
+    registryDepends: ["radio-group"],
+    files: [
+      {
+        path: "examples/radio-group-demo.tsx",
+        type: "registry:example",
+      },
+    ],
+  },
+  {
+    name: "textarea-demo",
+    type: "registry:example",
+    registryDepends: ["textarea"],
+    files: [
+      {
+        path: "examples/textarea-demo.tsx",
+        type: "registry:example",
+      },
+    ],
+  },
+  {
+    name: "textarea-disabled",
+    type: "registry:example",
+    registryDepends: ["textarea"],
+    files: [
+      {
+        path: "examples/textarea-disabled.tsx",
+        type: "registry:example",
+      },
+    ],
+  },
+  {
+    name: "textarea-with-button",
+    type: "registry:example",
+    registryDepends: ["textarea"],
+    files: [
+      {
+        path: "examples/textarea-with-button.tsx",
+        type: "registry:example",
+      },
+    ],
+  },
+  {
+    name: "textarea-with-label",
+    type: "registry:example",
+    registryDepends: ["textarea"],
+    files: [
+      {
+        path: "examples/textarea-with-label.tsx",
+        type: "registry:example",
+      },
+    ],
+  },
 ]
